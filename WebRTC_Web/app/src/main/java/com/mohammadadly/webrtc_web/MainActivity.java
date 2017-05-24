d5dc9c4d444b52d52a9c4c445b01d71a39a1a6fb
mohamedadly <eng.mohamed.adly@gmail.com>
2017-05-24 16:23:25 +0300

    - add permissions and settings

--- WebRTC_Web/app/src/main/java/com/mohammadadly/webrtc_web/MainActivity.java
+++ WebRTC_Web/app/src/main/java/com/mohammadadly/webrtc_web/MainActivity.java
@@ -1,13 +1,79 @@
 package com.mohammadadly.webrtc_web;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //WebView
+        WebView wv = (WebView) findViewById(R.id.a_webview);
+
+        //Setting
+        WebSettings webSettings = wv.getSettings();
+        // Enable Javascript
+        webSettings.setJavaScriptEnabled(true);
+
+        // Use WideViewport and Zoom out if there is no viewport defined
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        // Enable pinch to zoom without the zoom buttons
+        webSettings.setBuiltInZoomControls(true);
+
+        // Allow use of Local Storage
+        webSettings.setDomStorageEnabled(true);
+
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+            // Hide the zoom controls for HONEYCOMB+
+            webSettings.setDisplayZoomControls(false);
+        }
+
+        // Enable remote debugging via chrome://inspect
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        wv.setWebViewClient(new WebViewClient());
+
+        // AppRTC requires third party cookies to work
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptThirdPartyCookies(wv, true);
+
+        wv.loadUrl("https://www.searchandmap.com/cast/");
+
+        final MainActivity context = this;
+        wv.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                Log.d(TAG, "onPermissionRequest");
+                context.runOnUiThread(new Runnable() {
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void run() {
+                       // if(request.getOrigin().toString().equals("https://apprtc-m.appspot.com/")) {
+                            request.grant(request.getResources());
+                        //} else {
+                           // request.deny();
+                       // }
+                    }
+                });
+            }
+        });
     }
 }
